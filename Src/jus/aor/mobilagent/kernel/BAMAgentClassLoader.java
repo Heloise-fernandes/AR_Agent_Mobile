738849f1b1e97001c343670f9dea538c1d2ebbdf
Antoine BOISADAM <antoineboisadam@gmail.com>
2017-03-17 11:13:08 +0100

    BAMAgentClassLoader with dalleo

--- Src/jus/aor/mobilagent/kernel/BAMAgentClassLoader.java
+++ Src/jus/aor/mobilagent/kernel/BAMAgentClassLoader.java
@@ -1,30 +1,44 @@
 package jus.aor.mobilagent.kernel;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.jar.JarException;
+
 /**
- * #TODO
  * @author cheva
  *
  */
 public class BAMAgentClassLoader extends ClassLoader {
 
 	Jar code;
-	public BAMAgentClassLoader(String host, ClassLoader parent) {
+	public BAMAgentClassLoader(String jarName, ClassLoader parent) {
 		super(parent);
-		//TODO
-		
+		try {
+			integrateCode(new Jar(jarName));
+		} catch (JarException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public BAMAgentClassLoader(ClassLoader parent) {
 		super(parent);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public void integrateCode(Jar jar){
 		code = jar;
+		
+		// On define les classe une par une (On est sur que toutes seront utilisées)
+		for(Iterator<Map.Entry<String,byte[]>> iter = code.iterator(); iter.hasNext();) {
+			Map.Entry<String, byte[]> currentClass = iter.next();
+			this.defineClass(currentClass.getKey(), currentClass.getValue(), 0, currentClass.getValue().length);
+		}
 	}
 	
 	public String className(String name){
-		//TODO
-		return "";
+		return name.substring(0, name.length()-6).replace('/', '.');
 	}
 	
 	public Jar extractCode(){
@@ -32,7 +46,6 @@ public class BAMAgentClassLoader extends ClassLoader {
 	}
 	
 	public String toString(){
-		//TODO
-		return "BMA ";
+		return "BMA - " + code.toString();
 	}
 }
