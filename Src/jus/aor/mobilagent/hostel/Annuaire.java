60ec0df614b5cee6649c592e212f74761a173961
Antoine BOISADAM <antoineboisadam@gmail.com>
2017-04-04 14:25:07 +0200

    Annuaire.java

--- Src/jus/aor/mobilagent/hostel/Annuaire.java
+++ Src/jus/aor/mobilagent/hostel/Annuaire.java
@@ -1,21 +1,48 @@
 package jus.aor.mobilagent.hostel;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 import jus.aor.mobilagent.kernel.Agent;
 import jus.aor.mobilagent.kernel.Numero;
 import jus.aor.mobilagent.kernel._Action;
 import jus.aor.mobilagent.kernel._Annuaire;
 
-public class Annuaire extends Agent implements _Annuaire {
+public class Annuaire implements _Annuaire {
+	
+	private HashMap<String, Numero> annuaire = new HashMap<String, Numero>();
 
-	@Override
-	protected _Action retour() {
-		// TODO Auto-generated method stub
-		return null;
+	protected Annuaire(String fichier) throws ParserConfigurationException, SAXException, IOException{
+		
+		final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		    final DocumentBuilder builder = factory.newDocumentBuilder();       
+		    final Document document= builder.parse(new File( fichier ));
+			
+		    final NodeList elements = document.getElementsByTagName("Téléphone");
+
+			
+			for (int i = 0; i<elements.getLength(); i++) { // on boucle sur tous les noeuds et on remplis la hashmap
+				Node item = elements.item(i);
+			    String name = item.getAttributes().getNamedItem("name").getNodeName();
+			    Numero num = new Numero(item.getAttributes().getNamedItem("name").getNodeName());
+				this.annuaire.put(name, num);
+			
+			}
 	}
-
+	
+	@Override
 	public Numero get(String abonne) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.annuaire.get(abonne);
 	}
 
 }
