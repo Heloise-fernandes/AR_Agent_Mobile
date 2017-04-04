05f8715c41c2a1e0f6b5a1c6fe738070d65c8818
Antoine BOISADAM <antoineboisadam@gmail.com>
2017-04-04 14:31:36 +0200

    Chaine !!!! et Annuaire propre

--- Src/jus/aor/mobilagent/hostel/Annuaire.java
+++ Src/jus/aor/mobilagent/hostel/Annuaire.java
@@ -13,30 +13,36 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import jus.aor.mobilagent.kernel.Agent;
 import jus.aor.mobilagent.kernel.Numero;
-import jus.aor.mobilagent.kernel._Action;
 import jus.aor.mobilagent.kernel._Annuaire;
 
 public class Annuaire implements _Annuaire {
 
-	private HashMap<String, Numero> annuaire = new HashMap<String, Numero>();
+	private HashMap<String, Numero> annuaire;
 
-	protected Annuaire(String fichier) throws ParserConfigurationException, SAXException, IOException{
+	protected Annuaire(Object ...args) throws ParserConfigurationException, SAXException, IOException{
 
-		final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		final DocumentBuilder builder = factory.newDocumentBuilder();       
-		final Document document= builder.parse(new File( fichier ));
+		Document doc = null;
+		DocumentBuilder docBuilder;
+		try 
+		{
+			docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			doc = docBuilder.parse(new File((String) args[0]));
+		} catch (ParserConfigurationException | SAXException | IOException e) {e.printStackTrace();}
+		
+		this.annuaire = new HashMap<String, Numero>();
+		
+		if(doc!=null)
+		{
+			NodeList elements = doc.getElementsByTagName("Téléphone");
 
-		final NodeList elements = document.getElementsByTagName("Téléphone");
-
-
-		for (int i = 0; i<elements.getLength(); i++) { // on boucle sur tous les noeuds et on remplis la hashmap
-			Node item = elements.item(i);
-			String name = item.getAttributes().getNamedItem("name").getNodeName();
-			Numero num = new Numero(item.getAttributes().getNamedItem("name").getNodeName());
-			this.annuaire.put(name, num);
-
+			for (int i = 0; i<elements.getLength(); i++) { // on boucle sur tous les noeuds et on remplis la hashmap
+				Node item = elements.item(i);
+				String name = item.getAttributes().getNamedItem("name").getNodeName();
+				Numero num = new Numero(item.getAttributes().getNamedItem("name").getNodeName());
+				this.annuaire.put(name, num);
+	
+			}
 		}
 	}
 
